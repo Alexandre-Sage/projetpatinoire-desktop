5410b40151bad31b12026ca920aaaddaaeee4237
Alexandre <chorizosurffista@gmail.com>
2021-12-09 22:56:59 +0100

    recuperation town et country dans le state

--- src/components/InscriptionForm.jsx
+++ src/components/InscriptionForm.jsx
@@ -7,6 +7,7 @@ export default class InscriptionForm extends Component{
         this.state={
             countries:[],
             towns:[],
+            answers:{}
         }
         /*this.handleChange=this.handleChange.bind(this);*/
 
@@ -15,49 +16,58 @@ export default class InscriptionForm extends Component{
           .then(response => response.json())
           .then(data => this.setState({countries: [data]}));
 
-    } handleChange(value){
-        fetch(`http://localhost:4000/users/townsInscriptionForm/`+value.target.value)
+    } handleCountryChange(selectedCountry){
+        fetch(`http://localhost:4000/users/townsInscriptionForm/`+selectedCountry.target.value)
         .then(response => response.json())
         .then(data => this.setState({towns: [data]}))
+        this.setState({answers:{"country": selectedCountry.target.value}})
 
+    } handleTownChange(selectedTown){
+        this.setState({answers:{...this.state.answers, "town":selectedTown.target.value}})
+    } handleInputChange(){
+    
+    } handleSubmit(){
 
     } render(){
+        console.log("answers",this.state.answers);
+        console.log("town", this.state.towns);
         const countrySelectorJsx= this.state.countries.map((countries)=>countries.map((country)=><option key={country.countryId} value={country.countryId}>{country.countryName}</option>));
 
-        const townSelectroJsx= this.state.towns.map((towns)=>towns.map((town, key)=><option key={key} value={town.townId}>{town.townName}</option>));
+        const townSelectroJsx= this.state.towns.map((towns)=>towns.map((town, key)=><option key={key} value={town.townId}> {town.townName}</option>));
         return(
-            <div>
-
-                <form>
+            <div class="mainContInscriptionForm">
+                <form Class="inscriptionForm">
                     <label htmlFor="countriesSelector">Pays</label>
-                    <select onChange={(value)=>this.handleChange(value)} name="countriesSelector">
+                    <select Class="countriesSelector" onChange={(value)=>this.handleCountryChange(value)} name="countriesSelector">
+                        <option>Default</option>
                             {countrySelectorJsx}
                     </select>
 
-                    <label></label>
-                    <select>
+                    <label  htmlFor="townsSelector"></label>
+                    <select class="townsSelector" onChange={(value)=>this.handleTownChange(value)}>
                         <option>Default</option>
                         {townSelectroJsx}
                     </select>
 
                     <label htmlFor="firstName">Nom: </label>
-                    <input type="text" name="firstName"  />
+                    <input class="firstNameInput" type="text" name="firstName"  />
 
-                    <label htmlFor="lastName">Prénom: </label>
-                    <input type="text" name="lastName"  />
+                    <label  htmlFor="lastName">Prénom: </label>
+                    <input class="lasNameInput" type="text" name="lastName"  />
 
                     <label htmlFor="userName">Pseudo: </label>
                     <input type="text" name="userName"  />
 
                     <label htmlFor="eMail">Email: </label>
-                    <input type="text" name="eMail"/>
+                    <input class="mailInput" type="text" name="eMail"/>
 
                     <label htmlFor="password">Mot de Passe: </label>
-                    <input type="password" name="password"/>
+                    <input class="passwordInut" type="password" name="password"/>
 
                     <label htmlFor="passwordConfirmation">Confirmation Mot de Passe</label>
-                    <input type="password" name="passwordConfirmation"/>
-                    <button type="button">INSCRIPTION</button>
+                    <input class="passwordInut" type="password" value={this.value} name="passwordConfirmation"/>
+
+                    <button type="submit">INSCRIPTION</button>
                 </form>
             </div>
         )
